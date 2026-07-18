0f19e1b81dcdb9ed6a8a032350913e40cc8a5f5e
PROGRAMAGE <42641534+PROGRAMAGE@users.noreply.github.com>
2026-07-18 20:19:28 +0000

    Fixed firebase

--- plugins/firebase.client.ts
+++ plugins/firebase.client.ts
@@ -9,13 +9,14 @@ export default function initFirebase() {
   if (typeof window === 'undefined') return null
   if (firebaseApp) return firebaseApp
 
+  const config = useRuntimeConfig().public
   const firebaseConfig = {
-    apiKey: process.env.FIREBASE_API_KEY || '',
-    authDomain: process.env.FIREBASE_AUTH_DOMAIN || '',
-    projectId: process.env.FIREBASE_PROJECT_ID || '',
-    storageBucket: process.env.FIREBASE_STORAGE_BUCKET || '',
-    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID || '',
-    appId: process.env.FIREBASE_APP_ID || ''
+    apiKey: config.firebaseApiKey,
+    authDomain: config.firebaseAuthDomain,
+    projectId: config.firebaseProjectId,
+    storageBucket: config.firebaseStorageBucket,
+    messagingSenderId: config.firebaseMessagingSenderId,
+    appId: config.firebaseAppId
   }
 
   firebaseApp = initializeApp(firebaseConfig)
